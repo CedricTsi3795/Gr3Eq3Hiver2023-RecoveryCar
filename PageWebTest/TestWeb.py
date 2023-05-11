5b6a49d6b2665cddf6a038a934edcd0dd14380f0
DoffouJG <123177436+DoffouJG@users.noreply.github.com>
2023-05-11 18:41:49 -0400

    Update TestWeb.py
    remplacement de accueil par déconnexion sur les boutons dans les headers

--- PageWebTest/TestWeb.py
+++ PageWebTest/TestWeb.py
@@ -4,63 +4,22 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 # objectif : Permettre à l'équipe de pouvoir tester les pages web sans être obligé d'avoir le raspberry pi en possession
 
 Port = 8000
-# permet de savoir dans quelle page nous sommes actuellement
 
 # les pages :
 PAGEPHOTOS = """
 <html>
 
 <style>
-
 header {
-    background-color: 070707;
+    background-color: 	#C0C0C0;
     text-align: center;
 }
-
-.onglet {
-
-    border: 2px black inset ;
-    color: black;
-    padding: 8px 25px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
-    background-color: black;
-    color: white;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-
-}
-
-.titreDiv {
-    margin: 30px;
-    border: black double 10px;
-    text-align: center;
-    width: auto;
-    height: auto;
-    background-color: EAEAEA;
-}
-
-.galerieDiv {
-
-    margin: 40px;
-    text-align: center;
-    border: black groove 2px;
-    background-color: EAEAEA;
-
-    height: auto;
-    width: auto;
-
-}
-
 .photo {
 
     width: 225;
     height: 150;
 
-    margin: 30px;
+    margin: 20px;
 
     border: black 2px groove;
 
@@ -69,191 +28,10 @@ header {
 
 
 }
-
-.bouton {
-    border: 2px black outset ;
-    border-radius: 60px;
-    color: black;
-    padding: 8px 25px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 10px;
-    cursor: pointer;
-    background-color: DCDCDC;
-}
-
-
-
-
-</style>
-
-
-
-<head>
-    <meta charset="utf-8">
-    <title>RecoveryCar Live</title>
-
-    <! definir une image en favicon avec un lien au directory >
-    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
-</head>
-
-<! -- Header de la page avec les differents onglets disponibles sur le site -- >
-
-<header>
-
-    <form action="/" method="POST">            
-            <p>
-                <input class="onglet" type="submit" name="submit" value="Accueil">
-                <input class="onglet" type="submit" name="submit" value="Conduite">
-                <input class="onglet" type="submit" name="submit" value="Galerie">
-                
-            </p>
-           
-    </form>
-
-</header>
-
-<body style="background-color:C5C5C5">
-
-    <! -- Tite de la page -- >
-
-    <div class="titreDiv">
-        <h1 style="text-decoration: underline overline; font-size: 42px;">RecoveryCar</h1>
-        <h2 style="color: red;font-size: 30px; text-decoration: underline;">Live</h2>
-    </div>
-    
-    <! -- Division de la galerie photos -- >
-    
-    <div class="galerieDiv">
-
-        <div class="boutonsAction">
-
-            <h2 style="border: black 2px solid; border-left-width: 0px; border-right-width: 0px; background-color: DCDCDC; font-size: 24px;">Galerie photos</h2>
-
-        </div>
-
-
-        <p style="display: inline-block;">
-
-            <input class="bouton" type="submit" name="Sélectionner" value="Sélectionner" style="background-color: 0076FF;">
-            <input class="bouton" type="submit" name="Télécharger" value="Télécharger" style="background-color: 00FF59;">
-            <input class="bouton" type="submit" name="Supprimer" value="Supprimer" style="background-color: red;">
-            <input class="bouton" type="submit" name="Annuler" value="Annuler">
-
-        </p>
-
-        <p>
-            <img class="photo" >
-            <img class="photo" >
-            <img class="photo" >
-        </p>
-
-        <p>
-            <img class="photo" >
-            <img class="photo" >
-            <img class="photo" >
-        </p>
-
-        <p>
-            <img class="photo" >
-            <img class="photo" >
-            <img class="photo" >
-        </p>
-
-
-        <p style="text-align: center; display: inline-block;">
-
-            <input class="bouton" type="submit" name="Page précédente" value="&larr; Page précédente">
-
-            <p class="bouton" style="cursor: default;"> Page # / 10 </p>
-            
-                <input class="bouton" type="submit" name="Page suivante" value="Page suivante &rarr;">
-
-        </p>
-
-          
-        
-
-    </div>
-
-
-
-
-
-
-</body>
-</html>
-"""
-
-PAGECONTROLE = """
-<style>
-
-header {
-    background-color: 070707;
-    text-align: center;
-}
-
 .onglet {
 
-border: 2px black inset ;
-color: black;
-padding: 8px 25px;
-text-align: center;
-text-decoration: none;
-display: inline-block;
-font-size: 16px;
-margin: 4px 2px;
-cursor: pointer;
-background-color: black;
-color: white;
-font-family: Verdana, Geneva, Tahoma, sans-serif;
-
-}
-
-.titreDiv {
-    margin: 30px;
-    border: black double 10px;
-    text-align: center;
-    width: auto;
-    height: auto;
-    background-color: EAEAEA;
-}
-
-.cameraDiv {
-    margin: 10px;
-    margin-left: 50px;
-    margin-right: 40px;
-
-    width: 720px;
-    height: 560px;
-
-    border: black solid 3px;
-    text-align: center;
-    background-color: EAEAEA;
-    padding-bottom: 0px;
-    display: inline-block;
-}
-
-
-.mouvementDiv {
-    border: black ridge 3px;
-    text-align: center;
-
-    width: auto;
-    height: auto;
-
-    margin-top: 10px;
-
-    padding: 25px;
-    background-color: EAEAEA;
-    display: inline-block;
-}
-
-.bouton {
-    border: 2px black outset ;
-    border-radius: 60px;
+    border: 3px black inset ;
+    border-radius: 10px;
     color: black;
     padding: 8px 25px;
     text-align: center;
@@ -262,71 +40,195 @@ font-family: Verdana, Geneva, Tahoma, sans-serif;
     font-size: 16px;
     margin: 4px 2px;
     cursor: pointer;
-    background-color: DCDCDC;
+    background-color:#C0C0C0 ;
+    color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+
 }
 
+    .mouvementDiv {
+    border: black ridge 3px;
+    text-align: center;
+
+    width: auto;
+    height: auto;
+
+    margin-top: 40px;
+
+    padding: 8px;
+    background-color: #778899;
+    display: inline-block;
+}
 
 </style>
-
-
-
-
-
 <! -- Header de la page avec les differents onglets disponibles sur le site -- >
-
 <header>
-
-    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title></title>
     <form action="/" method="POST">            
             <p>
-                <input class="onglet" type="submit" name="submit" value="Accueil">
+                <input class="onglet" type="submit" name="submit" value="Deconnexion">
                 <input class="onglet" type="submit" name="submit" value="Conduite">
+                
+                
+            </p>
+           
+    </form>
+</header>
+
+
+<body style="background-color:#778899">
+<! -- division qui montre le logo -- >
+
+    <! -- Titre de la page -- >
+<div class="container-fluid p-5   text-center ">
+    
+        
+        <div class="container text-white bg-secondary p-3">
+        <dt style=";border: black px solid; border-left-width: 0px; border-right-width: 0px; background-color: #778899; font-size: 24px;">Galerie photo</dt>
+        <p style="display: inline-block;">
+
+            <input class="onglet" type="submit" name="Sélectionner" value="Sélectionner" style="background-color: 0076FF;">
+            <input class="onglet" type="submit" name="Télécharger" value="Télécharger" style="background-color: 00FF59;">
+            <input class="onglet" type="submit" name="Supprimer" value="Supprimer" style="background-color: red;">
+            <input class="onglet" type="submit" name="Annuler" value="Annuler">
+            <p>
+            <img class="photo" >
+            <img class="photo" >
+            <img class="photo" >
+        </p>
+
+        <p>
+            <img class="photo" >
+            <img class="photo" >
+            <img class="photo" >
+        </p>
+
+        <p>
+            <img class="photo" >
+            <img class="photo" >
+            <img class="photo" >
+        </p>
+
+        </p>
+        
+
+            
+        
+        </div>
+</div>
+    
+<! -- division qui regroupe la camera et les boutons -- >
+
+   
+
+
+
+</div>
+
+    
+</body>
+
+</html>
+"""
+
+PAGECONTROLE = """
+<style>
+header {
+    background-color: 	#C0C0C0;
+    text-align: center;
+}
+.onglet {
+
+    border: 3px black inset ;
+    border-radius: 10px;
+    color: black;
+    padding: 8px 25px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    background-color:#C0C0C0 ;
+    color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+
+}
+.bouton {
+    border: 2px black outset ;
+    border-radius: 50px;
+    color: black;
+    padding: 2px px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 13px;
+    margin: 2px;
+    cursor: pointer;
+    background-color: DCDCDC;
+}
+    .mouvementDiv {
+    border: black ridge 3px;
+    text-align: center;
+
+    width: auto;
+    height: auto;
+
+    margin-top: 40px;
+
+    padding: 8px;
+    background-color: #778899;
+    display: inline-block;
+}
+
+</style>
+<! -- Header de la page avec les differents onglets disponibles sur le site -- >
+<header>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title></title>
+    <form action="/" method="POST">            
+            <p>
+                <input class="onglet" type="submit" name="submit" value="Deconnexion">
                 <input class="onglet" type="submit" name="submit" value="Galerie">
                 
             </p>
            
     </form>
-
 </header>
 
 
-<body style="background-color:C5C5C5">
-
-    <! -- Tite de la page -- >
-
-    <div class="titreDiv">
-        <h1 style="text-decoration: underline overline; font-size: 42px;">RecoveryCar</h1>
+<body style="background-color:#778899">
+<! -- division qui montre le logo -- >
+<div class="container-fluid p-5 bg-secondary p-3 text-white text-center">
+        
+        <div class="container">
+        <h1 style="text-decoration: ; font-size: 42px;">RecoveryCar</h1>
         <h2 style="color: red;font-size: 30px; text-decoration: underline;">Live</h2>
+        <h3 style="text-decoration: ; font-size: 15px;">La suite entre vos mains</h1<3>
+        </div>
     </div>
+    <! -- Titre de la page -- >
+<div class="container-fluid p-5   text-center ">
     
-<! -- division qui regroupe la camera et les boutons -- >
-
-    <div style="text-align: center;">
-
-
-
-<! permet de controler le robot avec les touches du clavier >
-        <input type="hidden" name="keypressed" onkeypress="event">
-
-
-
-
-<! -- division d'image de la camera -- > 
-        <div class="cameraDiv">
-
-            <h2 style="border: black 2px solid; border-left-width: 0px; border-right-width: 0px; background-color: DCDCDC; font-size: 24px;">Image en temps réel</h2>
+        
+        <div class="container text-white bg-secondary p-3">
+        <dt style=";border: black px solid; border-left-width: 0px; border-right-width: 0px; background-color: #778899; font-size: 24px;">Image en temps réel</dt>
 
             <img src="stream.mjpg" width="630" height="420" style="border: black inset 2px;"/>
-
-        </div>
-
-
-<! -- division des boutons -- >        
+            <! -- division des boutons -- >  
+            <! -- nos differents boutons de deplacement -- >
         <div class="mouvementDiv">
 
             <form action="/" method="POST">
 
-<! -- nos differents boutons de deplacement -- > 
+ 
 
                 <p>
                     <input class="bouton" type="submit" name="Mode" value="Autonome" style="background-color: 0076FF;">
@@ -353,11 +255,22 @@ font-family: Verdana, Geneva, Tahoma, sans-serif;
             </form>
 
         </div>
+        </div>
+</div>
+    
+<! -- division qui regroupe la camera et les boutons -- >
+
+   
+
+
 
 </div>
 
     
 </body>
+
+
+
 """
 PAGEACCUEIL = """
 <html lang="en">
@@ -365,7 +278,7 @@ PAGEACCUEIL = """
         <form action="/" method="POST">
 
             <p>
-                <input class="bouton" type="submit" name="submit" value="Inscription">
+                <input class="bouton" type="submit" name="submit" value="Connexion">
             </p>
 
 
@@ -489,6 +402,7 @@ p {
 """
 
 
+# Classse qui va gérer le contenu affiché par les pages web
 class StreamingHandler(BaseHTTPRequestHandler):
     def _redirect(self, path):
         self.send_response(303)
@@ -526,25 +440,39 @@ class StreamingHandler(BaseHTTPRequestHandler):
             self.send_header('Content-Length', len(content))
             self.end_headers()
             self.wfile.write(content)
+        elif self.path == '/connexion.html':
+            # on ecrit nos pages ici
+            content = PAGECONNEXION.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
 
         else:
             self.send_error(404)
             self.end_headers()
 
     def do_POST(self):
+        l=1
         etat = ''
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode("utf-8")
         post_data = post_data.split("=")[1]
-        if post_data == 'Arriere' :
+        if post_data == 'Arriere':
             # appeler methode avant et etc en fct de ce qui a été cliqué
             etat = '/controle'
         elif post_data == 'Avant':
             # voici comment on va changer de page
             etat = '/controle'
-        elif post_data == 'Inscription':
+        elif post_data == 'Connexion':
             # voici comment on va changer de page
-            etat = '/controle'
+            etat = '/connexion'
+        
+        elif post_data == 'Inscription':
+            #créer une page d'inscription similaire à la page de connexion qui mettrait les infos dans la base de donnée
+            # voici comment on va changer de page
+            etat = '/Connexion'
         elif post_data == 'Gauche':
             # voici comment on va changer de page
             etat = '/controle'
@@ -561,35 +489,41 @@ class StreamingHandler(BaseHTTPRequestHandler):
             etat = '/controle'
         elif post_data == 'Conduite':
             etat = '/controle'
-        elif post_data == 'Accueil':
+        elif post_data == 'Deconnexion':
             etat = '/index'
         elif post_data == 'Galerie':
             etat = '/Galerie'
         elif post_data == 'Valider':
             etat = '/controle'
         else:
-          #verifie qu'un mot de passe a été écrit
+            #Mettre ça dans une méthode
             dernierscars=''
             longeur=len(post_data)
             counter = 0
-            for c in post_data:  # traverse the string “educative”
-                counter += 1  # increment the counter
+            for c in post_data:  
+                counter += 1  
                 if longeur-counter<4:
                     dernierscars+=c
             print("chars extraits sont",dernierscars)
             if dernierscars=="&psw":
-                etat='/controle'   
- 
+                etat='/controle'
+                print("wow")
+            else:  # je vais retirer cette ligne à la fin, c'est juste pour éviter que le site crash à chaque fois qu'on
+                # clique sur un bouton pour lequel on a encore rien prévu
+                print(post_data)
+                # self._redirect('/')
         print("RecoveryCar en Mode {}".format(post_data))
         print(etat)
         self.send_response(301)
-        # Ici, on envoit nos inforamtions au path auquel mène le bouton aui a été cliqué
+        # Ici, on envoie nos informations au path auquel mène le bouton aui a été cliqué
         self.send_header('Location', etat + '.html')
         self.end_headers()
 
 
+# Ouverture du serveur
+
 try:  # mettre votre propre adresse ip ici
-    ## dans cet exemple, il faut par exemple écrire sur google 192.168.2.35:8000
+    # dans cet exemple, il faut par exemple écrire sur google 192.168.2.35:8000
     server = HTTPServer(('192.168.2.35', Port), StreamingHandler)
     print('Started HTTPServer on port ', Port)
     server.serve_forever()
